5ad86ff35d87ebdb7ba1b88620d97f14b48b07dc
Kendrick Roy <roykendrick934@gmail.com>
2017-01-09 22:21:18 +0000

    my first letter on a grid

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
+/*global setColor*/
 //Write your function calls below this line
+setColor(0, 0, "green");
+setColor(1, 0, "green");
+setColor(2, 0, "green");
+setColor(3, 0, "green");
+setColor(4, 0, "green");
+setColor(5, 0, "green");
+setColor(6, 0, "green");
+setColor(7, 0, "green");
+setColor(8, 0, "green");
+setColor(9, 0, "green");
+setColor(5, 1, "green");
+setColor(4, 2, "green");
+setColor(3, 3, "green");
+setColor(2, 4, "green");
+setColor(1, 5, "green");
+setColor(0, 6, "green");
+setColor
+setColor()
+setColor()
